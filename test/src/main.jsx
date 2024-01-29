627e59f8c0f8f53be5c169d67f767f6ec80d2b01
MuhammadSaddique <saddiqueshah588@gmail.com>
2024-01-30 00:20:39 +0500

    add

--- test/src/main.jsx
+++ test/src/main.jsx
@@ -13,4 +13,11 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
-ReactDom.createRoot(document.get)
+
+
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <ReactFragment>
+    <h1>Hello World</h1>
+  </ReactFragment>
+)
