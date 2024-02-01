9959a664cf61d22e124f26c2cefacfcadf5c6913
MuhammadSaddique <saddiqueshah588@gmail.com>
2024-02-02 03:06:45 +0500

    more work

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -4,11 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const app = ()=>{
+    console.log(app,"app");
+  }
 
   return (
     <>
       <h1>Hello World</h1>
+      <button onClick={app}>Click</button>
+      
     </>
   )
 }
