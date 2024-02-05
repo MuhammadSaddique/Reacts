b908acb67426452ba912edb5c6c7d059fb1181fe
MuhammadSaddique <saddiqueshah588@gmail.com>
2024-02-06 00:46:04 +0500

    added

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Student from "./Student";
 import './app.css'
 
 function App() {
@@ -8,15 +9,27 @@ function App() {
   }
 
   return (
-    <>
+    <div className="App">
       <Header />
 
-      <h1>Main Body</h1>
-      <button onClick={app}>Click</button>
+      <Student
+        name="Saddique"
+        age={21}
+      />
+      <Student
+        name="Shafique"
+        age={18}
+      />
+      <Student
+        name="Rafique"
+        age={16}
+      />
+      {/* <h1>Main Body</h1>
+      <button onClick={app}>Click</button> */}
 
       <Footer />
 
-    </>
+    </div>
   )
 }
 
---
+++ test/src/Student.jsx
@@ -0,0 +1,11 @@
+const Student = (props) =>{
+
+    return(
+        <>
+        <h1>{props.name}</h1>
+        <h1>{props.age}</h1>
+        </>
+    )
+}
+
+export default Student;
