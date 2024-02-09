81b10efadba5d13ba559068e31bacae8d310257d
saddique <saddique@gmil.com>
2024-02-10 03:11:52 +0500

    added

--- useEffect/useEffect/src/App.jsx
+++ useEffect/useEffect/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState ,useEffect } from 'react'
+import React ,  { useState, useEffect } from 'react'
 
 import './App.css'
 
 function App() {
- 
+  useEffect(() => {
+    console.log("useEffect called");
+  })
+  console.log("Function Body");
+
   return (
     <>
-          </>
+      {console.log("inside jsx")}
+
+
+    </>
   )
 }
 
