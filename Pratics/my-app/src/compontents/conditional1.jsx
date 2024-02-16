2253b129ffa4b7e234a209fcd57b0eada3054713
saddique <saddique@gmil.com>
2024-02-16 19:54:24 +0500

    conditional rendering

---
+++ Pratics/my-app/src/compontents/conditional1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const conditional1 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default conditional1
---
+++ Pratics/my-app/src/compontents/conditional2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const conditional2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default conditional2
